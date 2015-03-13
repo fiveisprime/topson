8ebb94e2e40284777485579d79ab35e098f6f9a9
Matt Hernandez <matt@modulus.io>
2015-03-13 13:55:03 -0400

    better tests
    The tests should be able to run on Travis now.

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ SRC = index.js $(wildcard lib/*.js) $(wildcard spec/*.js)
 
 test: $(SRC)
 	@node_modules/.bin/jshint $^
-	@node_modules/.bin/istanbul test node_modules/.bin/_mocha \
+	@node_modules/.bin/istanbul cover node_modules/.bin/_mocha \
 		-R spec -- \
+		--require should \
 		--reporter spec
 
 .PHONY: test
--- spec/topson-spec.js
+++ spec/topson-spec.js
@@ -1,11 +1,38 @@
-var topson = require('../');
-var should = require('chai').should();
+var proxyquire = require('proxyquire');
+var sinon      = require('sinon');
+
+var procStub   = {};
+
+var topson = proxyquire('../lib/topson', { 'child_process': procStub });
+var parser = require('../lib/parser');
 
 describe('topson', function () {
-  it('should get top data', function () {
+  it('should get top data', function (done) {
+    procStub.exec = sinon.stub().yields(null, '{ "data": true }');
+
     topson(function (err, result) {
-      should.not.exist(err);
       result.should.exist;
+      done();
+    });
+  });
+
+  it('should handle errors', function (done) {
+    procStub.exec = sinon.stub().yields(new Error('failed'), null);
+
+    topson(function (err) {
+      err.should.exist;
+      done();
+    });
+  });
+});
+
+describe('parser', function () {
+  it('should parse the output', function (done) {
+    var out = require('fs').readFileSync('./spec/fixtures/out.txt');
+
+    parser.parse(out.toString(), 11, function (err, result) {
+      result.should.equal('[{"PID":"PID","COMMAND":"COMMAND","%CPU":"%CPU","TIME":"TIME","#TH":"#TH","#WQ":"#WQ","#PORTS":"#PORTS","MEM":"MEM","PURG":"PURG","CMPRS":"CMPRS","PGRP":"PGRP"},{"PID":"7355","COMMAND":"top","%CPU":"0.0","TIME":"00:00.11","#TH":"1/1","#WQ":"0","#PORTS":"13+","MEM":"1572K+","PURG":"0B","CMPRS":"0B","PGRP":"7355"}]');
+      done();
     });
   });
 });
